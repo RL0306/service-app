a3042665d3451ab0450816d74d126adbc209b537
RL0306 <lakhr036@gmail.com>
2022-05-16 10:59:03 +0100

    auth/prisma setup

--- sortza-app-project/src/auth/auth.controller.ts
+++ sortza-app-project/src/auth/auth.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
 
 @Controller("/api/v1/auth")
 export class AuthController {
 
-  @Post("/signup")
-  signUp() {
-    return "signing up";
+
+  constructor(private authService: AuthService) { }
+
+  @Post("/register")
+  register() {
+    return this.authService.register();
   }
 
 }
